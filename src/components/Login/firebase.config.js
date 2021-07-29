7f04f25d1ed20c72d726dc9745e809b36482276a
sanjidaafrin25 <sanjidaafrinmou70@gmail.com>
2021-07-29 19:00:44 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDhxZfSIC2HT0vQymOHfD5aUwf1KyBqNZA",
+  authDomain: "burj-al-arab-mim.firebaseapp.com",
+  projectId: "burj-al-arab-mim",
+  storageBucket: "burj-al-arab-mim.appspot.com",
+  messagingSenderId: "42868236481",
+  appId: "1:42868236481:web:5a1a272d73ca61000e7440",
+  measurementId: "G-MFY6XPB024"
+  };
+  export default firebaseConfig;
